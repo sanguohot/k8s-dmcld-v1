ae55ddd4eab8dd59c056285d8e119b438232f1fe
evan <281406674@qq.com>
2018-01-20 16:08:30 +0800

    增加simcloud配置 增加context.xml配置
    Signed-off-by: evan <281406674@qq.com>

--- dmcld-default/build_core.sh
+++ dmcld-default/build_core.sh
@@ -10,6 +10,6 @@ OUT_YAML_FILE=${OUT_YAML_PATH}/${IN_YAML_TEMPLATE_FILE/.template./.}
 
 mkdir -p ${OUT_YAML_PATH}
 
-config=`cat dmcld-default/${IN_CONFIG_FILE} && echo -e "\n" && cat ./${IN_CONFIG_FILE}`
+config=`cat dmcld-default/${IN_CONFIG_FILE} && echo -e "\nDMCLD_V1_DB_NAME=${DMCLD_V1_DB_NAME}\nDMCLD_V1_SERVER_ID=${DMCLD_V1_SERVER_ID}\n" && cat ./${IN_CONFIG_FILE}`
 templ=`cat dmcld-default/${IN_YAML_TEMPLATE_FILE}`
 printf "$config\ncat << EOF\n$templ\nEOF" | bash > ${OUT_YAML_FILE}
