be0f707d828fd568e290046d932208cae2bad280
evan <281406674@qq.com>
2018-03-15 16:01:22 +0800

    修改支持qnsoft
    Signed-off-by: evan <281406674@qq.com>

--- dmcld-default/build_core.sh
+++ dmcld-default/build_core.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-source config
-IN_CONFIG_FILE=config
-source dmcld-default/${IN_CONFIG_FILE}
+IN_CONFIG_FILE=config_other
 DMCLD_V1_SERVER_ID=$1
-DMCLD_V1_DB_OTHER="other"
-DMCLD_V1_DB_TEST="test"
-if [ ${DMCLD_V1_SERVER_ID} = ${DMCLD_V1_DB_OTHER} ] ; then
-    DMCLD_V1_DB_NAME="simserver"
-elif [ ${DMCLD_V1_SERVER_ID} = ${DMCLD_V1_DB_TEST} ] ; then
-    DMCLD_V1_DB_NAME="test111"
+if [ ${DMCLD_V1_SERVER_ID} = "qnsoft" ] ; then
+    IN_CONFIG_FILE=config_qnsoft
+elif [ ${DMCLD_V1_SERVER_ID} = "test" ] ; then
+    IN_CONFIG_FILE=config_test
+elif [ ${DMCLD_V1_SERVER_ID} = "other" ] ; then
+    IN_CONFIG_FILE=config_other
 else
-    DMCLD_V1_DB_NAME=$DMCLD_V1_BASE-$DMCLD_V1_SERVER_ID
+    IN_CONFIG_FILE=config_other
 fi
 IN_YAML_TEMPLATE_FILE=$2
 OUT_YAML_PATH=.tmp/$DMCLD_V1_BASE-$DMCLD_V1_SERVER_ID
@@ -18,6 +16,9 @@ OUT_YAML_FILE=${OUT_YAML_PATH}/${IN_YAML_TEMPLATE_FILE/.template./.}
 
 mkdir -p ${OUT_YAML_PATH}
 
-config=`cat dmcld-default/${IN_CONFIG_FILE} && echo -e "\nDMCLD_V1_DB_NAME=${DMCLD_V1_DB_NAME}\nDMCLD_V1_SERVER_ID=${DMCLD_V1_SERVER_ID}\n" && cat ./${IN_CONFIG_FILE}`
+config=`cat dmcld-default/${IN_CONFIG_FILE} \
+&& cat dmcld-default/config_util \
+#&& echo -e "\nDMCLD_V1_DB_NAME=${DMCLD_V1_DB_NAME}\nDMCLD_V1_SERVER_ID=${DMCLD_V1_SERVER_ID}\n" \
+&& cat ./config`
 templ=`cat dmcld-default/${IN_YAML_TEMPLATE_FILE}`
 printf "$config\ncat << EOF\n$templ\nEOF" | bash > ${OUT_YAML_FILE}
