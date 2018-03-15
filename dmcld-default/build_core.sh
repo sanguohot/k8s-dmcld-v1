6ccd58a5fb1fe44ed44e0eb23898ec0ea20999b3
evan <281406674@qq.com>
2018-03-15 16:49:03 +0800

    bug修复，同时调整默认代理副本为1
    Signed-off-by: evan <281406674@qq.com>

--- dmcld-default/build_core.sh
+++ dmcld-default/build_core.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source config
 IN_CONFIG_FILE=config_other
 DMCLD_V1_SERVER_ID=$1
 if [ ${DMCLD_V1_SERVER_ID} = "qnsoft" ] ; then
@@ -16,9 +17,6 @@ OUT_YAML_FILE=${OUT_YAML_PATH}/${IN_YAML_TEMPLATE_FILE/.template./.}
 
 mkdir -p ${OUT_YAML_PATH}
 
-config=`cat dmcld-default/${IN_CONFIG_FILE} \
-&& cat dmcld-default/config_util \
-#&& echo -e "\nDMCLD_V1_DB_NAME=${DMCLD_V1_DB_NAME}\nDMCLD_V1_SERVER_ID=${DMCLD_V1_SERVER_ID}\n" \
-&& cat ./config`
+config=`cat dmcld-default/${IN_CONFIG_FILE} && echo -e "\n" && cat dmcld-default/config_util && echo -e "\nDMCLD_V1_SERVER_ID=${DMCLD_V1_SERVER_ID}\n" && cat config`
 templ=`cat dmcld-default/${IN_YAML_TEMPLATE_FILE}`
 printf "$config\ncat << EOF\n$templ\nEOF" | bash > ${OUT_YAML_FILE}
